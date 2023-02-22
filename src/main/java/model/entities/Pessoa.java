163401eb6fe9fe5eaf3f17124ca817c5cb48b1bc
Felipefams <augustofams@gmail.com>
2023-02-22 15:41:33 -0300

    end

--- src/main/java/model/entities/Pessoa.java
+++ src/main/java/model/entities/Pessoa.java
@@ -4,14 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
+
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Pessoa implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    //we could use @Column(name = "x") to change the db column's
     private String nome;
     private String email;
 
